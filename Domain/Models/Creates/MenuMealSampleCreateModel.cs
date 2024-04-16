10e0a947a2b4a6b5a0ac5d9ed38669f4c3818552
Janglee <votantai4899@gmail.com>
2024-04-16 17:56:18 +0700

    menu sample id for post

--- Domain/Models/Creates/MenuMealSampleCreateModel.cs
+++ Domain/Models/Creates/MenuMealSampleCreateModel.cs
@@ -2,6 +2,7 @@
 {
     public class MenuMealSampleCreateModel
     {
+        public Guid MenuSampleId { get; set; }
         public string Name { get; set; } = null!;
         public TimeSpan From { get; set; }
         public TimeSpan To { get; set; }
--- Domain/Models/Views/BirdCategoryViewModel.cs
+++ Domain/Models/Views/BirdCategoryViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class BirdCategoryViewModel
     {
+        public Guid Id { get; set; }
         public string? ThumbnailUrl { get; set; }
         public string Name { get; set; } = null!;
     }
